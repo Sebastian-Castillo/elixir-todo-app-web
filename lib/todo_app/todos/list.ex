a26ad7c4d0e7c334c9d27a492fbec27f21ac1050
scastil <scastil@bancolombia.com.co>
2022-09-21 22:28:18 -0500

    relacion entre listas y tareas

--- lib/todo_app/todos/list.ex
+++ lib/todo_app/todos/list.ex
@@ -2,10 +2,18 @@ defmodule TodoApp.Todos.List do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias TodoApp.Accounts.User
+  alias TodoApp.Todos.TaskList
+  alias TodoApp.Todos.Task
+
   schema "lists" do
     field :name, :string
     field :tags, {:array, :string}
-    field :user_id, :id
+    belongs_to :user, User
+
+    has_many :collaborators, User
+
+    many_to_many :tasks, Task, join_through: TaskList
 
     timestamps()
   end
@@ -13,7 +21,9 @@ defmodule TodoApp.Todos.List do
   @doc false
   def changeset(list, attrs) do
     list
-    |> cast(attrs, [:name, :tags])
-    |> validate_required([:name, :tags])
+    |> cast(attrs, [:name, :tags, :user_id])
+    |> validate_required([:name, :tags, :user_id])
+    |> foreign_key_constraint(:user_id)
+    |> cast_assoc(:tasks, with: &TaskList.changeset/2)
   end
 end
